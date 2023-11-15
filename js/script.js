19ebd7321dffcd088ab689f7477c3a22fa02fc43
lee_c <gitleelee@gmail.com>
2023-11-14 18:54:36 -0800

    Added event handlers, function expressions, and querySelectorAll() to pot luck list.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-// invite button
-const addGuestButton = document.querySelector(".invite");
-// label for the invite button
-const guestInputLabel = document.querySelector(".add-guest label");
-// text input box
-const guestInput = document.querySelector(".add-guest input");
-// unordered list (not yet visible)
-const guestList = document.querySelector(".guest-list");
-// span class for number of guests attending
-const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
-const guestFull = document.querySelector(".alert");
+    // invite button
+    const addGuestButton = document.querySelector(".invite");
+    // label for the invite button
+    const guestInputLabel = document.querySelector(".add-guest label");
+    // text input box
+    const guestInput = document.querySelector(".add-guest input");
+    // unordered list (not yet visible)
+    const guestList = document.querySelector(".guest-list");
+    // span class for number of guests attending
+    const guestCount = document.querySelector(".attendance");
+    // alert when guest list is full (not yet visible)
+    const guestFull = document.querySelector(".alert");
+
+    addGuestButton.addEventListener("click", function () {    
+        const guest = guestInput.value;
+        // console.log(guest);
+        if (guest !== "") {
+          addToList(guest);
+          updateGuestCount();
+          // Clear the Input Box
+          clearInput(); 
+        }
+      });
+
+      const clearInput = function () {  
+        guestInput.value = "";
+      };
+
+      const addToList = function (guest) {
+      const listItem = document.createElement("li");
+      listItem.innerText = guest;
+          guestList.append(listItem);
+      };
+
+      const updateGuestCount = function () {
+        const guests = document.querySelectorAll(".guest-list li");
+        guestCount.innerText = guests.length;
+        // Limit the Guest List
+        if (guests.length === 8) { 
+          addGuestButton.classList.add("hide");
+          guestInput.classList.add("hide");
+          guestInputLabel.classList.add("hide");
+          guestFull.classList.remove("hide");
+        };
+      };
